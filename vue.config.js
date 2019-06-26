a000b3288c85db0491a5d5da370e62f6bc08fcf9
Griffin <griffinlqjones@gmail.com>
2019-06-26 16:47:25 -0700

    Test index landing

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/docs/' : '/',
-  outputDir: 'docs'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/DirtFarmClicker/'
+    : '/'
 }
